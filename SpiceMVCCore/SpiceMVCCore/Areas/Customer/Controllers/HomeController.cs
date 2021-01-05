50faad181cddf3d51a4b4744c22b578cc7059407
rekhaguntamadugu <72582299+rekhaguntamadugu@users.noreply.github.com>
2021-01-05 13:17:09 -0500

    Landing Page added
    Landing Page and some Users Registration files

--- SpiceMVCCore/SpiceMVCCore/Areas/Customer/Controllers/HomeController.cs
+++ SpiceMVCCore/SpiceMVCCore/Areas/Customer/Controllers/HomeController.cs
@@ -2,28 +2,45 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using SpiceMVCCore.Data;
 using SpiceMVCCore.Models;
+using SpiceMVCCore.Models.ViewModels;
+using SpiceMVCCore.Utility;
 
 namespace SpiceMVCCore.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext_db _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext_db db)
         {
-            _logger = logger;
+            _db = db;
         }
 
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            IndexViewModel IndexVM = new IndexViewModel()
+            {
+                MenuItem = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync(),
+                Category = await _db.Category.ToListAsync(),
+                Coupon = await _db.Coupon.Where(c => c.IsActive == true).ToListAsync()
+
+            };
+         
+            return View(IndexVM);
         }
 
+
+     
         public IActionResult Privacy()
         {
             return View();
